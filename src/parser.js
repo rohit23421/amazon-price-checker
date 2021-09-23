72676e4762fa334875110afda09e572659a52ad2
Rohit sah <61084486+rohit23421@users.noreply.github.com>
2021-09-23 12:06:11 +0530

    Add files via upload

---
+++ src/parser.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API);
+
+const nightmare = require("nightmare")();
+
+const args = process.argv.slice(2);
+const url = args[0];
+const minPrice = args[1];
+
+checkPrice();
+
+async function checkPrice() {
+  try {
+    const priceString = await nightmare
+      .goto(url)
+      .wait("#priceblock_ourprice")
+      .evaluate(() => document.getElementById("priceblock_ourprice").innerText)
+      .end();
+    const priceNumber = parseFloat(priceString.replace("$", ""));
+    if (priceNumber < minPrice) {
+      sendEmail(
+        "Price is low",
+        `The price on ${url} has dropped below ${minPrice}`
+      );
+    }
+  } catch (e) {
+    sendEmail("Amazon Price Checker Error", e.message);
+    throw e;
+  }
+}
+
+function sendEmail(subject, body) {
+  const email = {
+    to: `yinof74303@vefblogg.com`,
+    from: `amazon-price-checker@example.com`,
+    subject: subject,
+    text: body,
+    html: body,
+  };
+  return sgMail.send(email);
+}
